cedc18c50f13f66e3f85f319620db1c004f2572e
Sam Zilli <samzilli01@gmail.com>
2024-08-05 22:03:49 -0400

    fixed typo in path name

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import cors from "cors";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
-import AssignmentRoutes from "./kanbas/Assignments/routes.js";
+import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 
 const app = express();
 app.use(cors());                    // make sure cors is used right after creating the app

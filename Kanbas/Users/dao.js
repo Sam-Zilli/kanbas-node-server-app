85d11f2945c2bdec740078120c09620a825df709
Sam Zilli <samzilli01@gmail.com>
2024-08-10 10:29:26 -0400

    starting 3.6.3

--- Kanbas/Users/dao.js
+++ Kanbas/Users/dao.js
@@ -9,3 +9,11 @@ export const deleteUser = (userId) => model.deleteOne({ _id: userId });
 
 
 export const findUsersByRole = (role) => model.find({ role: role }); // or just model.find({ role })
+
+export const findUsersByPartialName = (partialName) => {
+    const regex = new RegExp(partialName, "i"); // 'i' makes it case-insensitive
+    return model.find({
+      $or: [{ firstName: { $regex: regex } }, { lastName: { $regex: regex } }],
+    });
+  };
+  

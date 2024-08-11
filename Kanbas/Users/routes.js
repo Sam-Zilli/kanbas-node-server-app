2ffd1725e6f6cd1c73930d71bb240caccf3eb14f
Sam Zilli <samzilli01@gmail.com>
2024-08-11 09:38:43 -0400

    Starting 5.5.2

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -1,13 +1,10 @@
 import * as dao from "./dao.js";
-let currentUser = null;
 
 export default function UserRoutes(app) {
-
   const createUser = async (req, res) => {
     const user = await dao.createUser(req.body);
     res.json(user);
   };
-
 
   const deleteUser = async (req, res) => {
     const status = await dao.deleteUser(req.params.userId);
@@ -32,7 +29,6 @@ export default function UserRoutes(app) {
     return;
   };
 
-
   const findUserById = async (req, res) => {
     const user = await dao.findUserById(req.params.userId);
     res.json(user);
@@ -47,42 +43,46 @@ export default function UserRoutes(app) {
   const signup = async (req, res) => {
     const user = await dao.findUserByUsername(req.body.username);
     if (user) {
-      res.status(400).json(
-        { message: "Username already taken" });
+      res.status(400).json({ message: "Username already taken" });
       return;
     }
-    currentUser = await dao.createUser(req.body);
+    const currentUser = await dao.createUser(req.body);
+    req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
-
 
   const signin = async (req, res) => {
     const { username, password } = req.body;
-    currentUser = await dao.findUserByCredentials(username, password);
-    res.json(currentUser);
+    const currentUser = await dao.findUserByCredentials(username, password);
+    if (currentUser) {
+      req.session["currentUser"] = currentUser;
+      res.json(currentUser);
+    } else {
+      res.status(401).json({ message: "Unable to login. Try again later." });
+    }
   };
 
+  const signout = (req, res) => {
+    req.session.destroy();
+    res.sendStatus(200);
+  };
 
-const signout = (req, res) => {
-  currentUser = null;
-  res.sendStatus(200);
-};
-
-
-  
-
-    const profile = async (req, res) => {
-      res.json(currentUser);
-    };
-  
-
+  const profile = (req, res) => {
+    const currentUser = req.session["currentUser"];
+    console.log("Current User is: ", currentUser)
+    if (!currentUser) {
+      res.sendStatus(401);
+      return;
+    }
+    res.json(currentUser);
+  };
 
   app.post("/api/users", createUser);
   app.get("/api/users", findAllUsers);
   app.get("/api/users/:userId", findUserById);
   app.put("/api/users/:userId", updateUser);
   app.delete("/api/users/:userId", deleteUser);
-  
+
   app.post("/api/users/signup", signup);
   app.post("/api/users/signin", signin);
   app.post("/api/users/signout", signout);

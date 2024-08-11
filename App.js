f971f7f5c10c6db4b90084b52d2135ea1a1cb082
Sam Zilli <samzilli01@gmail.com>
2024-08-11 18:48:56 -0400

    before moving to root

--- App.js
+++ App.js
@@ -20,10 +20,24 @@ app.use(cors({
 );
 
 const sessionOptions = {
-  secret: "any string",
+  secret: process.env.SESSION_SECRET || "kanbas",
   resave: false,
   saveUninitialized: false,
 };
+
+
+// if NOT developing
+if (process.env.NODE_ENV !== "development") {
+  sessionOptions.proxy = true;
+  sessionOptions.cookie = {
+    sameSite: "none",
+    secure: true,
+    domain: process.env.NODE_SERVER_DOMAIN,
+  };
+}
+
+
+
 app.use(
   session(sessionOptions)
 );

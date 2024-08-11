04c5d95110c7248fdc9cd3b065c9d239a2f91b3e
Sam Zilli <samzilli01@gmail.com>
2024-08-11 16:43:13 -0400

    found bug

--- App.js
+++ App.js
@@ -18,20 +18,16 @@ app.use(cors({
   origin: process.env.NETLIFY_URL || "http://localhost:3000",
 })
 );
+
 const sessionOptions = {
-secret: process.env.SESSION_SECRET || "kanbas",
-resave: false,
-saveUninitialized: false,
+  secret: "any string",
+  resave: false,
+  saveUninitialized: false,
 };
-if (process.env.NODE_ENV !== "development") {
-sessionOptions.proxy = true;
-sessionOptions.cookie = {
-  sameSite: "none",
-  secure: true,
-  domain: process.env.NODE_SERVER_DOMAIN,
-};
-}
-app.use(session(sessionOptions));
+app.use(
+  session(sessionOptions)
+);
+
 
 
 app.use(express.json());

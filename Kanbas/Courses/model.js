da135056b6faeb1b805b20c63ae0565c11a2c40c
Sam Zilli <samzilli01@gmail.com>
2024-08-11 16:52:08 -0400

    adding courses from dao

--- Kanbas/Courses/model.js
+++ Kanbas/Courses/model.js
@@ -1,14 +1,4 @@
 import mongoose from 'mongoose';
-
-const courseSchema = new mongoose.Schema({
-  number: String,
-  name: String,
-  startDate: String, 
-  endDate: String,   
-  department: String,
-  credits: Number,
-  description: String,
-});
-
+import courseSchema from "./schema.js";
 const Course = mongoose.model('Course', courseSchema);
 export default Course;
--- Kanbas/Users/model.js
+++ Kanbas/Users/model.js
@@ -1,24 +1,5 @@
 import mongoose from "mongoose";
-
-const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    firstName: String,
-    email: String,
-    lastName: String,
-    dob: Date,
-    role: {
-      type: String,
-      enum: ["STUDENT", "FACULTY", "ADMIN", "USER"],
-      default: "USER",
-    },
-    loginId: String,
-    section: String,
-    lastActivity: Date,
-    totalActivity: String,
-  },
-  { collection: "users" }
-);
+import userSchema from "./schema.js";
 
 const User = mongoose.model("UserModel", userSchema);
 export default User;

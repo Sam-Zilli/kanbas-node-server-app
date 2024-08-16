1f531113472e9718290826162621e5ea5bd423e2
Sam Zilli <samzilli01@gmail.com>
2024-08-16 10:45:45 -0400

    added type and default type to quiz schema

--- Kanbas/Quizzes/schema.js
+++ Kanbas/Quizzes/schema.js
@@ -1,9 +1,18 @@
 import mongoose from "mongoose";
 
 const questionSchema = new mongoose.Schema({
+  type: { 
+    type: String, 
+    enum: ["multiple_choice", "true_false", "short_answer", "fill_in_the_blank"], 
+    default: "multiple_choice" 
+  },
   name: { type: String, required: true },
   description: { type: String, required: false},
-  question: { type: String, required: true }
+  options: { type: [String], required: function() { return this.type === "multiple_choice"; } }, 
+  correctAnswer: { 
+    type: mongoose.Schema.Types.Mixed, 
+    required: true 
+  }
 }, { _id: false }); 
 
 const quizSchema = new mongoose.Schema({
